72833fc34707f10f9945f59285efaff70c38851e
zoetototro <enoshima.tototro2@gmail.com>
2020-05-13 19:51:43 +0900

    no message

--- App/screens/profile.js
+++ App/screens/profile.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 import { AuthContext } from "./../context";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginVertical: 10,
-    borderRadius: 5,
   },
 });
 
@@ -24,8 +16,12 @@ export const Profile = ({ navigation }) => {
 
   return (
     <ScreenContainer>
-      <Text>Profile Screen</Text>
-      <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
+      <Image
+        source={{
+          uri: "https://source.unsplash.com/user/chrisjoelcampbell",
+        }}
+        style={{ maxHeight: 320, width: null, flex: 1 }}
+      />
       <Button title="Sign Out" onPress={() => signOut()} />
     </ScreenContainer>
   );
